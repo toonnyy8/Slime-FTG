da447e58218208b5214aebcb982f31eda28cdced
toonnyy8 <toonnyy8@gmail.com>
2019-06-20 12:51:17 +0800

    init

---
+++ src/js/main.js
@@ -0,0 +1,99 @@
+import "@babel/polyfill"
+
+import fs from 'fs'
+
+import * as BABYLON from './babylon-module'
+
+let slime = fs.readFileSync(__dirname + '../../../file/slime.glb')
+
+let url = {
+    "slime": URL.createObjectURL(new Blob([slime]))
+}
+
+let loader = new BABYLON.GLTFFileLoader()
+
+console.log()
+// Get the canvas DOM element
+let canvas = document.getElementById('bobylonCanvas')
+// Load the 3D engine
+let engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true })
+// CreateScene function that creates and return the scene
+function createScene() {
+
+    // This creates a basic Babylon Scene object (non-mesh)
+    let scene = new BABYLON.Scene(engine)
+    //Adding a light
+    let light = new BABYLON.HemisphericLight()
+
+    //Adding an Arc Rotate Camera
+    let camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 10, BABYLON.Vector3.Zero(), scene)
+    camera.attachControl(canvas, false)
+
+    let animationGroup
+    BABYLON.SceneLoader.OnPluginActivatedObservable.addOnce(loader => {
+        loader.animationStartMode = BABYLON.GLTFLoaderAnimationStartMode.NONE
+    })
+
+    let assetsManager = new BABYLON.AssetsManager(scene)
+    let meshTask = assetsManager.addMeshTask("Character", "", "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/AnimatedMorphCube/glTF-Binary/", "AnimatedMorphCube.glb")
+    meshTask.onSuccess = (task) => {
+        animationGroup = scene.animationGroups[0] //.start(false)
+
+        animationGroup.start(true)
+        animationGroup.goToFrame(0)
+        animationGroup.pause()
+    }
+    meshTask.onError = (task, message, exception) => {
+        console.log(message, exception)
+    }
+    assetsManager.load()
+
+    let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI")
+
+    let panel = new BABYLON.GUI.StackPanel()
+    panel.width = "220px"
+    panel.rotation = 0
+    panel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT
+    panel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER
+    advancedTexture.addControl(panel)
+
+    panel.addControl(slider)
+
+    let slider = new BABYLON.GUI.Slider()
+    slider.minimum = 0
+    slider.maximum = 4.199997425079346
+    slider.value = 0
+    slider.paddingTop = "10px"
+    slider.height = "30px"
+    slider.width = "200px"
+    slider.color = "green"
+    slider.onValueChangedObservable.add((value) => {
+        animationGroup.goToFrame(value)
+        animationGroup.pause()
+        console.log(value)
+    })
+
+    panel.addControl(slider)
+
+
+    BABYLON.SceneLoader.ImportMesh("", url["slime"], "", scene, (meshes, particleSystems, skeletons, animationGroups) => {
+        console.log(scene.animationGroups[1])
+        //scene.animationGroups[1].goToFrame(7 / 24)
+        //scene.animationGroups[1].pause()
+    }, null, null, ".glb")
+
+    return scene
+
+}
+// call the createScene function
+let scene = createScene()
+// run the render loop
+engine.runRenderLoop(() => {
+    scene.render()
+
+})
+// the canvas/window resize event handler
+window.addEventListener('resize', () => {
+    engine.resize()
+})
+
